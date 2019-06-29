a25ad4843a86f5f2ed7de0495a2f38477caace9f
scotnewbury <scot@scotnewbury.com>
2019-06-29 01:02:46 -0400

    Added KNEX and functionality to add user to db

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt-nodejs')
 const cors = require('cors')
 const knex = require('knex')
 
-const postgres = knex ({
+const db = knex ({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
@@ -14,7 +14,6 @@ const postgres = knex ({
   }
 });
 
-console.log(postgres.select('*').from ('users'));
 
 const app = express()
 app.use(bodyParser.json())
@@ -56,16 +55,11 @@ app.post('/signin', (req,res) => {
 
 app.post('/register', (req, res) => {
   const {email, name, password} = req.body
-  bcrypt.hash(password, null, null, function(err, hash) {
-    console.log(hash);
-  });
-  database.users.push({
-    id: '125',
-    name: name,
+  db('users').insert({
     email: email,
-    entries: 0,
+    name: name,
     joined: new Date()
-  })
+  }).then(console.log())
   res.json(database.users[database.users.length - 1])
 })
 
