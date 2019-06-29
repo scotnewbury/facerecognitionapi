cd830f6ca4d1d837f71b18e75d2a1546ad87a8c0
scotnewbury <scot@scotnewbury.com>
2019-06-29 15:26:51 -0400

    Update profile endpoint to pull data from database

--- server.js
+++ server.js
@@ -71,18 +71,17 @@ app.post('/register', (req, res) => {
 
 app.get('/profile/:id', (req, res) => {
   const {id} = req.params
-  let found = false
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true
-      return res.json(user)
-    }
-  })
-
-  if (!found) {
-    res.status(400).json('no such user')
-  }
-
+  db.select('*').from('users').
+    where({id}).
+    then(user => {
+      if (user.length) {
+        res.json(user[0])
+      } else {
+        res.status(400).json('No such user')
+      }
+      
+    })
+    .catch(err => res.status(400).json('Error getting user'))
 })
 
 app.put('/image', (req, res) => {
