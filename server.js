470fd6cbed9cca6b276dee1e418a791a44ee4244
scotnewbury <scot@scotnewbury.com>
2019-06-29 15:39:08 -0400

    Update image endpoint so entry count and be incremented

--- server.js
+++ server.js
@@ -86,20 +86,15 @@ app.get('/profile/:id', (req, res) => {
 
 app.put('/image', (req, res) => {
   const {id} = req.body
-  let found = false
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true
-      user.entries++
-      return res.json(user.entries)
-    }
+  db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    res.json(entries[0]);
   })
-
-  if (!found) {
-    res.status(400).json('no such user')
-  }
-
+  .catch(err => res.status(400).json('Unable to update entry count'))
 })
+
 
 app.listen (3000, () => {
   console.log('app is running on port 3000')
