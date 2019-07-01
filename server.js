c07b3fd0c6150b0becf36321bb98228f9098566e
scotnewbury <scot@scotnewbury.com>
2019-07-01 15:10:40 -0400

    changing response

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const db = knex ({
   connection: {
     host : '127.0.0.1',
     user : 'scot',
-    password : 'scot',
+    password : '',
     database : 'smart-brain'
   }
 });
@@ -24,10 +24,7 @@ const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send(database.users)
-})
-
+app.get('/', (req, res) => {res.send('Yup, it\'s working')})
 app.post('/signin', (req, res) => {signin.handleSignIn(req, res, db, bcrypt)}) 
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
