d785d2be9430111932fc8dbd3946cb4e14230392
scotnewbury <scot@scotnewbury.com>
2019-06-30 23:46:00 -0400

    Added form checks and moved API key to backend

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require('clarifai')
+
+const app = new Clarifai.App ({
+  apiKey: '5065782b15ec42c280323caa0d49208d'
+})
+
+const handleApiCall = (req, res) => {
+app.models
+  .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+  .then (data => {
+    res.json(data)
+  })
+  .catch (err => res.status(400).json('Unable to work with API'))
+}
+
 const handleImage = (req, res, db) => {
   const {id} = req.body
   db('users').where('id', '=', id)
@@ -10,5 +25,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-  handleImage: handleImage
+  handleImage,
+  handleApiCall
 }
